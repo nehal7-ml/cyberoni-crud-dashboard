477d31042c22b5bbcabfeac7109c87d68e7b4ae3
nehal7-ml <nehal.sk.99@gmail.com>
2024-03-22 16:39:26 +0530

    added info modal for indexing request

--- components/IndexingRequest/index.tsx
+++ components/IndexingRequest/index.tsx
@@ -1,41 +1,67 @@
-'use client'
-import { InfoIcon, SearchCheck } from "lucide-react";
+"use client";
+import { InfoIcon, SearchCheck, X } from "lucide-react";
 import Tooltip from "../shared/ToolTip";
 import Modal from "../shared/Modal";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { indexEverything } from "./submit";
 import Loading from "../Loading";
+import Notification, { toast } from "../Notification";
+import LoadingDots from "../shared/loading-dots";
+import Link from "next/link";
 
 function IndexingRequest() {
-    const [showProgress, setShowProgress] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [infoModal, setInfoModal] = useState(false);
     const [progress, setProgress] = useState(0);
 
-    async function requestIndexing() {
-        setShowProgress(prev=> !prev);
-        const res = await indexEverything();
+    async function requestIndexing(event: FormEvent) {
+        event.preventDefault()
+        setLoading((prev) => !prev);
+        try {
+            await indexEverything();
+        } catch (error) {
+            toast((error as Error).toString(), {
+                type: 'error'
+            })
+            console.error("Error occurred while indexing:", error);
+        }
 
-        setShowProgress(prev=> !prev);
+        toast('Requested indexing successfully ', {
+            type: 'success'
+        })
+        setLoading((prev) => !prev);
     }
 
-    useEffect(() => {
-        console.log(showProgress);
-    }, [showProgress]);
     return (
-        <div>
-            <form action={requestIndexing}>
+        <div className="z-30">
+            <form onSubmit={requestIndexing}>
                 <Tooltip text="Index all blogs and services">
-                    <button type="submit" className="flex rounded-md p-2 shadow-md">        <SearchCheck />
-                        Request Indexing
-                    </button>
+                    <div className="flex gap-2">
+                        <button type="submit" className="flex rounded-md p-2 shadow-md">
+                            <SearchCheck />
+                            Request Indexing
+                        </button>
+                        <button onClick={() => setInfoModal((prev) => !prev)}><InfoIcon /></button>
+                    </div>
                 </Tooltip>
-
             </form>
-            <Modal show={showProgress} setShow={setShowProgress}>
-                <div className="w-fit">
-                    <Loading />
+            <Modal show={infoModal} setShow={setInfoModal}>
+                <div className="relative w-fit bg-gray-50 rounded-md flex flex-col justify-center items-center">
+                    <button onClick={() => setInfoModal((prev) => !prev)} className="absolute hover:text-red-500 top-5 right-5"><X></X></button>
+                    <div className="flex container p-5 flex-col justify-center items-center text-center">
+                        <InfoIcon className="h-10 w-10 text-gray-500" />
+                        <span>the time it takes for Google to index blog pages varies significantly based on several factors, including the size of the website, the quality of the content, and the website's crawl budget.</span>
+                        <span>Maximum requests by default is 200/day add quota in <Link className="underline text-blue-400" target="_blank" href={'https://console.cloud.google.com/projectselector2/apis/dashboard?organizationId=0&supportedpurview=project'}>Cloud console </Link> for more</span>
+                        <span>View Indexing information about you site on the <Link className="underline text-blue-400" href={"https://search.google.com/search-console"} target="_blank" >Search Console</Link> </span>
+                    </div>
                 </div>
             </Modal>
-        </div>);
+            {loading && <div className="z-[9999]">
+                <Loading />
+            </div>}
+            <Notification />
+        </div>
+    );
 }
 
 export default IndexingRequest;
--- components/Loading/index.tsx
+++ components/Loading/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Loading() {
   return (
-    <div className="w-ful fixed left-0 right-0 top-0 flex h-full items-center justify-center  backdrop-blur-sm">
+    <div className="w-ful fixed left-0 right-0 top-0 flex h-full items-center justify-center  backdrop-blur-sm z-[9999]">
       <div className="h-16 w-16 animate-spin rounded-full border-t-4 border-solid border-blue-500"></div>
     </div>
   );
