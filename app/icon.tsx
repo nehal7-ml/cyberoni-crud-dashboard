8fa621e4066f45e40f70649d5848afd224ed06e8
nehal7-ml <nehal.sk.99@gmail.com>
2024-05-01 02:35:43 +0530

    remove runtime

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,13 +1,12 @@
 import { getBaseUrl } from "@/lib/utils";
 import { ImageResponse } from "next/og";
-// Route segment config
-export const runtime = "nodejs";
+
 // Image metadata
 export const size = {
   width: 32,
   height: 32,
 };
-export const contentType = "image/webp";
+export const contentType = "image/png";
 
 // Image generation
 export default function Icon() {
