d8062d7214c1c840db65adbc404d92159b7e2bdd
nehal7-ml <nehal.sk.99@gmail.com>
2024-06-21 18:22:28 +0530

    update packages

--- app/api/softwares/add/route.ts
+++ app/api/softwares/add/route.ts
@@ -7,11 +7,9 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 async function post(req: Request) {
-  if (req.method === "POST") {
     const product = (await req.json()) as CreateSoftwareProductDTO;
     const newProduct = await create(product, prisma);
     return NextResponse.json({ message: "Add success", data: newProduct });
-  }
 }
 
 export const { POST, DELETE, GET, PATCH, PUT } = apiHandler({ POST: post });
